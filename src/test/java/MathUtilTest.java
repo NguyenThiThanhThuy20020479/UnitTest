52e09526bfb80addee1c89d99bc62d1dcb6d875c
NguyenThiThanhThuy20020479 <80307287+NguyenThiThanhThuy20020479@users.noreply.github.com>
2022-10-15 20:26:41 +0700

    fixerror

--- src/test/java/MathUtilTest.java
+++ src/test/java/MathUtilTest.java
@@ -10,138 +10,138 @@ public class MathUtilTest {
     // Các test của bảng quyết định
     @Test
     void test1(){
-        assertEquals(util.testTritangle(1,1,3),"Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác",util.testTritangle(1,1,3));
     }
     @Test
     void test2(){
-        assertEquals(util.testTritangle(1,3,1),"Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(1,3,1));
     }
 
     @Test
     void test3(){
-        assertEquals(util.testTritangle(3,1,1),"Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(3,1,1));
     }
     @Test
     void test4(){
-        assertEquals(util.testTritangle(-10.5, 10, 5.5), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(-10.5, 10, 5.5) );
     }
     @Test
     void test5(){
-        assertEquals(util.testTritangle(10, -10.5, 5.5), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(10, -10.5, 5.5) );
     }
     @Test
     void test6(){
-        assertEquals(util.testTritangle(10, 5.5, -10.5), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(10, 5.5, -10.5));
     }
     @Test
     void test7(){
-        assertEquals(util.testTritangle(3, 4, 5), "Tam giác vuông");
+        assertEquals("Tam giác vuông", util.testTritangle(3, 4, 5));
     }
     @Test
     void test8(){
-        assertEquals(util.testTritangle(4, 3, 5), "Tam giác vuông");
+        assertEquals("Tam giác vuông",util.testTritangle(4, 3, 5));
     }
     @Test
     void test9(){
-        assertEquals(util.testTritangle(5, 3, 4), "Tam giác vuông");
+        assertEquals("Tam giác vuông", util.testTritangle(5, 3, 4));
     }
     @Test
     void test10(){
-        assertEquals(util.testTritangle(4, 4, 4 * Math.sqrt(2)), "Tam giác vuông cân");
+        assertEquals( "Tam giác vuông cân", util.testTritangle(4, 4, 4 * Math.sqrt(2)));
     }
     @Test
     void test11(){
-        assertEquals(util.testTritangle(4, 4 * Math.sqrt(2), 4), "Tam giác vuông cân");
+        assertEquals("Tam giác vuông cân", util.testTritangle(4, 4 * Math.sqrt(2), 4));
     }
     @Test
     void test12(){
-        assertEquals(util.testTritangle(4 * Math.sqrt(2), 4, 4), "Tam giác vuông cân");
+        assertEquals("Tam giác vuông cân", util.testTritangle(4 * Math.sqrt(2), 4, 4));
     }
     @Test
     void test13(){
-        assertEquals(util.testTritangle(50, 20, 50), "Tam giác cân");
+        assertEquals("Tam giác cân", util.testTritangle(50, 20, 50));
     }
     @Test
     void test14(){
-        assertEquals(util.testTritangle(10, 20, 20), "Tam giác cân");
+        assertEquals("Tam giác cân", util.testTritangle(10, 20, 20));
     }
     @Test
     void test15(){
-        assertEquals(util.testTritangle(100.2, 100.2, 80), "Tam giác cân");
+        assertEquals("Tam giác cân", util.testTritangle(100.2, 100.2, 80));
     }
     @Test
     void test16(){
-        assertEquals(util.testTritangle(10.4, 10.4, 10.4), "Tam giác đều");
+        assertEquals("Tam giác đều", util.testTritangle(10.4, 10.4, 10.4));
     }
     @Test
     void test17(){
-        assertEquals(util.testTritangle(4.1, 5.1, 7.1), "Tam giác thường");
+        assertEquals("Tam giác thường", util.testTritangle(4.1, 5.1, 7.1));
     }
 
     // các test của kiểm thử giá trị biên
     @Test
     void test19() {
-        assertEquals(util.testTritangle(nom, nom, min), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, nom, min));
 
     }
     @Test
     void test20() {
-        assertEquals(util.testTritangle(nom, nom, minAdd), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, nom, minAdd));
 
     }
     @Test
     void test21() {
-        assertEquals(util.testTritangle(nom, nom, max), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, nom, max));
 
     }
     @Test
     void test22() {
-        assertEquals(util.testTritangle(nom, nom, maxSub), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, nom, maxSub));
 
     }
     @Test
     void test23() {
-        assertEquals(util.testTritangle(nom, min, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, min, nom));
 
     }
     @Test
     void test24() {
-        assertEquals(util.testTritangle(nom, minAdd, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, minAdd, nom));
 
     }
     @Test
     void test25() {
-        assertEquals(util.testTritangle(nom, max, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, max, nom));
 
     }
     @Test
     void test26() {
-        assertEquals(util.testTritangle(nom, maxSub, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, maxSub, nom));
 
     }
     @Test
     void test27() {
-        assertEquals(util.testTritangle(min, nom, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(min, nom, nom));
 
     }
     @Test
     void test28() {
-        assertEquals(util.testTritangle(minAdd, nom, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(minAdd, nom, nom));
 
     }
     @Test
     void test29() {
-        assertEquals(util.testTritangle(max, nom, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(max, nom, nom));
 
     }
     @Test
     void test30() {
-        assertEquals(util.testTritangle(maxSub, nom, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(maxSub, nom, nom));
 
     }
     @Test
     void test31() {
-        assertEquals(util.testTritangle(nom, nom, nom), "Đây không phải 3 cạnh của một tam giác");
+        assertEquals("Đây không phải 3 cạnh của một tam giác", util.testTritangle(nom, nom, nom));
 
     }
 
